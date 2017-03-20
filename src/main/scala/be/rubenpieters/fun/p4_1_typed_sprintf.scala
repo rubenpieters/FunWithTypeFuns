55c05165790a1266e6ba95852be63428d49ebede
ruben <rubnepieters@gmail.com>
2017-03-20 22:59:40 +0100

    apparently annotating the type works

--- src/main/scala/be/rubenpieters/fun/p4_1_typed_sprintf.scala
+++ src/main/scala/be/rubenpieters/fun/p4_1_typed_sprintf.scala
@@ -48,7 +48,7 @@ object p4_1_typed_sprintf extends App {
 
 
 
-  /*implicit def tPrinterCmp[X, F1, F2, OF1, OF2]
+  implicit def tPrinterCmp[X, F1, F2, OF1, OF2]
   (implicit printerF1: TPrinter.Aux[F1, OF2, OF1]
    , printerF2: TPrinter.Aux[F2, X, OF2]
   ): TPrinter.Aux[C[F1, F2], X, OF1] = new TPrinter[C[F1, F2], X] {
@@ -62,86 +62,6 @@ object p4_1_typed_sprintf extends App {
 
       print1
     }
-  }*/
-
-  implicit def tPrinterCmp1[X]
-  (implicit printerF1: TPrinter.Aux[L, X, X]
-   , printerF2: TPrinter.Aux[L, X, X]
-  ): TPrinter.Aux[C[L, L], X, X] = new TPrinter[C[L, L], X] {
-    type Out = X
-
-    override def print(f: F[C[L, L]], k: (String) => X): X = {
-      val cmp = f.asInstanceOf[Cmp[L, L]]
-      def endStr(s1: String, s2: String): X = k(s1 + s2)
-      def print2(s1: String) = printerF2.print(cmp.f2, s2 => endStr(s1, s2))
-      def print1 = printerF1.print(cmp.f1, s1 => print2(s1))
-
-      print1
-    }
-  }
-
-  implicit def tPrinterCmp2[X, Val1]
-  (implicit printerF1: TPrinter.Aux[V[Val1], X, Val1 => X]
-   , printerF2: TPrinter.Aux[L, X, X]
-  ): TPrinter.Aux[C[V[Val1], L], X, Val1 => X] = new TPrinter[C[V[Val1], L], X] {
-    type Out = Val1 => X
-
-    override def print(f: F[C[V[Val1], L]], k: (String) => X): Val1 => X = {
-      val cmp = f.asInstanceOf[Cmp[V[Val1], L]]
-      def endStr(s1: String, s2: String): X = k(s1 + s2)
-      def print2(s1: String) = printerF2.print(cmp.f2, s2 => endStr(s1, s2))
-      def print1 = printerF1.print(cmp.f1, s1 => print2(s1))
-
-      print1
-    }
-  }
-
-  implicit def tPrinterCmp3[X, Val1]
-  (implicit printerF1: TPrinter.Aux[L, Val1 => X, Val1 => X]
-   , printerF2: TPrinter.Aux[V[Val1], X, Val1 => X]
-  ): TPrinter.Aux[C[L, V[Val1]], X, Val1 => X] = new TPrinter[C[L, V[Val1]], X] {
-    type Out = Val1 => X
-
-    override def print(f: F[C[L, V[Val1]]], k: (String) => X): Val1 => X = {
-      val cmp = f.asInstanceOf[Cmp[L, V[Val1]]]
-      def endStr(s1: String, s2: String): X = k(s1 + s2)
-      def print2(s1: String) = printerF2.print(cmp.f2, s2 => endStr(s1, s2))
-      def print1 = printerF1.print(cmp.f1, s1 => print2(s1))
-
-      print1
-    }
-  }
-
-  implicit def tPrinterCmp4[X, Val1, Val2]
-  (implicit printerF1: TPrinter.Aux[V[Val1], Val2 => X, Val1 => Val2 => X]
-   , printerF2: TPrinter.Aux[V[Val2], X, Val2 => X]
-  ): TPrinter.Aux[C[V[Val1], V[Val2]], X, Val1 => Val2 => X] = new TPrinter[C[V[Val1], V[Val2]], X] {
-    type Out = Val1 => Val2 => X
-
-    override def print(f: F[C[V[Val1], V[Val2]]], k: (String) => X): Val1 => Val2 => X = {
-      val cmp = f.asInstanceOf[Cmp[V[Val1], V[Val2]]]
-      def endStr(s1: String, s2: String): X = k(s1 + s2)
-      def print2(s1: String) = printerF2.print(cmp.f2, s2 => endStr(s1, s2))
-      def print1 = printerF1.print(cmp.f1, s1 => print2(s1))
-
-      print1
-    }
-  }
-
-  implicit def tPrinterCmp5[X, Val1, F1, F2, O2]
-  (implicit printerF1: TPrinter.Aux[V[Val1], O2, Val1 => O2]
-   , printerF2: TPrinter.Aux[C[F1, F2], X, O2]
-  ): TPrinter.Aux[C[V[Val1], C[F1, F2]], X, Val1 => O2] = new TPrinter[C[V[Val1], C[F1, F2]], X] {
-    type Out = Val1 => O2
-
-    override def print(f: F[C[V[Val1], C[F1, F2]]], k: (String) => X): Val1 => O2 = {
-      val cmp = f.asInstanceOf[Cmp[V[Val1], C[F1, F2]]]
-      def endStr(s1: String, s2: String): X = k(s1 + s2)
-      def print2(s1: String) = printerF2.print(cmp.f2, s2 => endStr(s1, s2))
-      def print1 = printerF1.print(cmp.f1, s1 => print2(s1))
-
-      print1
-    }
   }
 
   val f_ld = Lit("day")
@@ -149,16 +69,19 @@ object p4_1_typed_sprintf extends App {
   val val_int = Val[Int](null, _.toString)
   val f_dn: F[C[L, V[Int]]] = Cmp(Lit("day "), val_int)
   val f_nds: F[C[V[Int], C[L, L]]] = Cmp(val_int, Cmp(Lit(" day"), Lit("s")))
+  val f_nds2: F[C[V[Int], C[V[Int], L]]] = Cmp(val_int, Cmp(val_int, Lit("s")))
 
   implicitly[TPrinter.Aux[L, String, String]]
   implicitly[TPrinter.Aux[L, Int => String, Int => String]]
   implicitly[TPrinter.Aux[V[Int], String, Int => String]]
-  println(TPrinter[C[L, L], String].print(f_lds, identity))
+  //println(TPrinter[C[L, L], String].print(f_lds, identity))
   //println(TPrinter[C[L, V[Int]], String].print(f_dn, identity)(5))
   println(f_ld.sprintf)
   println(val_int.sprintf.apply(5))
   //println(f_nds.sprintf.apply(5))
   //println(f_lds.sprintf)
+  println(implicitly[TPrinter.Aux[C[V[Int], C[V[Int], L]], String, Int => Int => String]].print(f_nds2, identity).apply(5).apply(6))
+  println(f_nds2.sprintf[Int => Int => String].apply(5).apply(6))
 
   implicit class printerL[A](f: F[A]) {
     def print[X, O](k: String => X)(implicit tp: TPrinter.Aux[A, X, O]): O = tp.print(f, k)
