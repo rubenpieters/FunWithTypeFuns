93834acd6ce8c5f5198fa13f974a86eeae6aed3c
ruben <rubnepieters@gmail.com>
2017-03-20 23:00:31 +0100

    remove old code

--- src/main/scala/be/rubenpieters/fun/p4_1_typed_sprintf.scala
+++ src/main/scala/be/rubenpieters/fun/p4_1_typed_sprintf.scala
@@ -37,17 +37,6 @@ object p4_1_typed_sprintf extends App {
     override def print(f: F[V[Val_]], k: (String) => X): Val_ => X = (x: Val_) => k(f.asInstanceOf[Val[Val_]].printer(x))
   }
 
-  sealed trait Func[O1, O2] {
-    type Out
-  }
-
-  object Func {
-    type Aux[O1, O2, Out0] = Func[O1, O2] { type Out = Out0 }
-    def apply[O1, O2](implicit ev: Func[O1, O2]): Aux[O1, O2, ev.Out] = ev
-  }
-
-
-
   implicit def tPrinterCmp[X, F1, F2, OF1, OF2]
   (implicit printerF1: TPrinter.Aux[F1, OF2, OF1]
    , printerF2: TPrinter.Aux[F2, X, OF2]
@@ -74,12 +63,8 @@ object p4_1_typed_sprintf extends App {
   implicitly[TPrinter.Aux[L, String, String]]
   implicitly[TPrinter.Aux[L, Int => String, Int => String]]
   implicitly[TPrinter.Aux[V[Int], String, Int => String]]
-  //println(TPrinter[C[L, L], String].print(f_lds, identity))
-  //println(TPrinter[C[L, V[Int]], String].print(f_dn, identity)(5))
   println(f_ld.sprintf)
   println(val_int.sprintf.apply(5))
-  //println(f_nds.sprintf.apply(5))
-  //println(f_lds.sprintf)
   println(implicitly[TPrinter.Aux[C[V[Int], C[V[Int], L]], String, Int => Int => String]].print(f_nds2, identity).apply(5).apply(6))
   println(f_nds2.sprintf[Int => Int => String].apply(5).apply(6))
 
