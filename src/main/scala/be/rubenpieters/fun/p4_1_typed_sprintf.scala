de280150e27ce8c98d60d4a8696a69211d8826cf
ruben <rubnepieters@gmail.com>
2017-03-21 20:21:49 +0100

    change representation of 4_1

--- src/main/scala/be/rubenpieters/fun/p4_1_typed_sprintf.scala
+++ src/main/scala/be/rubenpieters/fun/p4_1_typed_sprintf.scala
@@ -1,76 +1,63 @@
 package be.rubenpieters.fun
 
 object p4_1_typed_sprintf extends App {
+  sealed trait F
+  case class Lit(str: String) extends F
+  case class Val[A](printer: A => String) extends F
+  case class Cmp[F1 <: F, F2 <: F](f1: F1, f2: F2) extends F
 
-  sealed trait F[A]
-  case class Lit(str: String) extends F[L]
-  case class Val[Val_](parser: Parser[Val_], printer: Printer[Val_]) extends F[V[Val_]]
-  case class Cmp[F1, F2](f1: F[F1], f2: F[F2]) extends F[C[F1, F2]]
+  val valInt = Val[Int](_.toString)
+  val valStr = Val[String](identity)
+  val nameAge = Cmp(Cmp(Lit("name="), valStr), Cmp(Lit(", age="), valInt))
 
-  sealed trait L
-  sealed trait V[Val]
-  sealed trait C[F1, F2]
-
-  type Parser[A] = String => List[(A, String)]
-  type Printer[A] = A => String
-
-  sealed trait TPrinter[A, X] {
+  sealed trait TPrinter[A <: F, X] {
     type Out
 
-    def print(f: F[A], k: String => X): Out
+    def print(f: A, k: String => X): Out
   }
 
   object TPrinter {
-    type Aux[F, X, Out0] = TPrinter[F, X] { type Out = Out0 }
-    def apply[F, X](implicit ev: TPrinter[F, X]): Aux[F, X, ev.Out] = ev
+    type Aux[A <: F, X, Out0] = TPrinter[A, X] { type Out = Out0 }
   }
 
-  implicit def tPrinterL[X]: TPrinter.Aux[L, X, X] = new TPrinter[L, X] {
+  implicit def tPrinterLit[X]: TPrinter.Aux[Lit, X, X] = new TPrinter[Lit, X] {
     type Out = X
 
-    override def print(f: F[L], k: (String) => X): X = k(f.asInstanceOf[Lit].str)
+    override def print(f: Lit, k: (String) => X): X = k(f.str)
   }
 
-  implicit def tPrinterVal[X, Val_]: TPrinter.Aux[V[Val_], X, Val_ => X] = new TPrinter[V[Val_], X] {
-    type Out = Val_ => X
+  implicit def tPrinterVal[X, A]: TPrinter.Aux[Val[A], X, A => X] = new
+      TPrinter[Val[A], X] {
+    type Out = A => X
 
-    override def print(f: F[V[Val_]], k: (String) => X): Val_ => X = (x: Val_) => k(f.asInstanceOf[Val[Val_]].printer(x))
+    override def print(f: Val[A], k: (String) => X): A => X =
+      (x: A) => k(f.printer(x))
   }
 
-  implicit def tPrinterCmp[X, F1, F2, OF1, OF2]
+  implicit def tPrinterCmp[X, F1 <: F, F2 <: F, OF1, OF2]
   (implicit printerF1: TPrinter.Aux[F1, OF2, OF1]
    , printerF2: TPrinter.Aux[F2, X, OF2]
-  ): TPrinter.Aux[C[F1, F2], X, OF1] = new TPrinter[C[F1, F2], X] {
+  ): TPrinter.Aux[Cmp[F1, F2], X, OF1] = new TPrinter[Cmp[F1, F2], X] {
     type Out = OF1
 
-    override def print(f: F[C[F1, F2]], k: (String) => X): OF1 = {
-      val cmp = f.asInstanceOf[Cmp[F1, F2]]
+    override def print(f: Cmp[F1, F2], k: (String) => X): OF1 = {
       def endStr(s1: String, s2: String): X = k(s1 + s2)
-      def print2(s1: String) = printerF2.print(cmp.f2, s2 => endStr(s1, s2))
-      def print1 = printerF1.print(cmp.f1, s1 => print2(s1))
+      def print2(s1: String) = printerF2.print(f.f2, s2 => endStr(s1, s2))
+      def print1 = printerF1.print(f.f1, s1 => print2(s1))
 
       print1
     }
   }
 
-  val f_ld = Lit("day")
-  val f_lds: F[C[L, L]] = Cmp(Lit("day"), Lit("s"))
-  val val_int = Val[Int](null, _.toString)
-  val f_dn: F[C[L, V[Int]]] = Cmp(Lit("day "), val_int)
-  val f_nds: F[C[V[Int], C[L, L]]] = Cmp(val_int, Cmp(Lit(" day"), Lit("s")))
-  val f_nds2: F[C[V[Int], C[V[Int], L]]] = Cmp(val_int, Cmp(val_int, Lit("s")))
+  println(implicitly[TPrinter.Aux[Cmp[Cmp[Lit, Val[String]], Cmp[Lit,
+    Val[Int]]], String, String => Int => String]].print(nameAge, identity)("ruben")(23))
 
-  implicitly[TPrinter.Aux[L, String, String]]
-  implicitly[TPrinter.Aux[L, Int => String, Int => String]]
-  implicitly[TPrinter.Aux[V[Int], String, Int => String]]
-  println(f_ld.sprintf)
-  println(val_int.sprintf.apply(5))
-  println(implicitly[TPrinter.Aux[C[V[Int], C[V[Int], L]], String, Int => Int => String]].print(f_nds2, identity).apply(5).apply(6))
-  println(f_nds2.sprintf[Int => Int => String].apply(5).apply(6))
-
-  implicit class printerL[A](f: F[A]) {
-    def print[X, O](k: String => X)(implicit tp: TPrinter.Aux[A, X, O]): O = tp.print(f, k)
-    def sprintf[O](implicit tp: TPrinter.Aux[A, String, O]): O = tp.print(f, identity)
+  implicit class printerOps[A <: F](f: A) {
+    def print[X, O](k: String => X)(implicit tp: TPrinter.Aux[A, X, O]):
+    O = tp.print(f, k)
+    def sprintf[O](implicit tp: TPrinter.Aux[A, String, O]): O =
+      tp.print(f, identity)
   }
 
+  println(nameAge.sprintf[String => Int => String].apply("ruben")(23))
 }
